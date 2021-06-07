7d0c151078ab03de22ea3923f17643ae432efbbd
Agwa <agwa.dan@gmail.com>
2021-06-07 23:02:46 +0300

    Create todo list smart contract.

---
+++ contracts/TodoList.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+/* Initializing the contract */
+contract TodoList {
+    uint256 public taskCount = 0; //-------------------------> State variables -> represent the state of the smart contract on the blockchain
+}
