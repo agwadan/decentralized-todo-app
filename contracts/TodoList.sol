4d29cbcecd133b0b912a48ef77442fc377c4d937
Agwa <agwa.dan@gmail.com>
2021-06-10 14:19:14 +0300

    Add a function to create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 /* Initializing the contract */
 contract TodoList {
     uint256 public taskCount = 0; //-------------------------> State variables -> represent the state of the smart contract on the blockchain
+
+    /* Model for the tasks. */
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("default task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
